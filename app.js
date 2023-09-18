da22134eb3d73fccbcd7deb1449d138caee03e46
muhammad-harisdk <142913590+muhammad-harisdk@users.noreply.github.com>
2023-09-18 00:46:55 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,107 +1,120 @@
-// ******************************Q1*************************************
+// ********************************Q1*****************************************
 
 for(var i = 1; i <= 10; i++){
     console.log(i);
 }
 
-// ******************************Q2*************************************
+// ********************************Q2*****************************************
 
-for(var i = 1; i <=12; i++){
-    console.log("Cake",i);
+var userinput = +prompt("Enter a number to show its multiplication table")
+
+var userinputlength = +prompt("Enter length multiplication table")
+
+for(var i = 1; i <= userinputlength; i++){
+    document.write(userinput + "X" + i + "=" + userinput*i + "<br>")
+}
+// ********************************Q3*****************************************
+
+var fruits = ["apple","banana","mango","orange","strawberry"]
+
+// document.write(fruits[0],"<br>",fruits[1],"<br>",fruits[2],"<br>",fruits[3],"<br>",fruits[4])
+
+for(var i = 0; i <= fruits.length; i++){
+    document.write("Element at index " + i +" is "+ fruits[i] +"<br>")
 }
 
-// ******************************Q3*************************************
+// ********************************Q4*****************************************
 
-for(var i = 0; i <= 4; i++){
-    console.log();
-}
-
-// ******************************Q4*************************************
-
-for(var a = 1; a <= 100; a++){
-    document.write(a,",")
-}
-
-// ******************************Q5*************************************
-
-document.write("<br>")
-
-for(var i = 100; i >= 1; i--){
-    document.write(i,",")
-}
-
-// ******************************Q6*************************************
-
-var arr = [12,45,68,312,85]
-
-document.write("<br>",arr)
-
-// ******************************Q7*************************************
-
-var flag = (false,true)
-
-document.write("<br>",flag)
-
-// ******************************Q8*************************************
-
-var animal = ["dog","Elephant","Tiger","Lion"]
-
-for(var i = 0; i <animal.length;i++){
-    document.write("<br>",i,animal[i])
-}
-
-// ******************************Q9*************************************
+document.write("<b>","Counting","</b>","<br>")
 
 for(var i = 1; i <= 10; i++){
-    console.log(i);
+    document.write(i ,",")
+}
+document.write("<br>","<b>","Reverse Counting","</b>","<br>")
+
+for(var b = 10; b >= 1; b--){
+    document.write(b + ",")
 }
 
-for(var a = 0; a <= 2; a++){
-    alert("Q9 is very difficult")
-}
+document.write("<br>","<b>","Even Number","</b>","<br>")
 
-// ******************************Q10*************************************
-
-// var firstName = prompt("Enter your first name")
-
-// var lastName = prompt("Enter your Last name")
-
-// var arr1 = firstName + lastName
-
-// var username = prompt("Enter user name")
-
-// if (arr1 === username) {
-//     alert("Enter")
-// }
-// else{
-//     alert("Please write correct user name")
-// }
-
-// ******************************Q11*************************************
-
-var list = ["Karachi","Lahore","Quetta"]
-
-var userInput = prompt("Enter your live")
-
-for(var i = 0; i <list.length; i++){
-    if (userInput === list[i]) {
-        alert("Match Found");
-        matchFound = true; break;
-    }else{
-        alert("Match Not Found")
-        matchnotfound = true;  break;
+for (var i = 0; i <= 20; i++) {
+    if (i % 2 == 0) {
+      document.write(i,",");
     }
 }
 
-// ******************************Q12*************************************
+document.write("<br>","<b>","Odd Number","</b>","<br>")
 
-document.write("<br>")
+for (var i = 0; i <= 20; i++) {
+    if (i % 2 != 0) {
+      document.write(i,",")
+    }
+}
 
-var firstArr = ["a","b","c","d","e","f"]
+document.write("<br>","<b>","Series","</b>","<br>")
 
-var secondArr = [1,2,3,4,5,6]
+for (var i = 2; i <= 20; i++) {
+    if (i % 2 == 0) {
+      document.write(i,"k",",");
+    }
+}
 
-for(var i = 0; i < firstArr.length;i++){
-    for(var a = 0; a < secondArr.length;a++)
-    document.write("<br>",firstArr[i],secondArr[a])
+// ********************************Q5*****************************************
+
+document.write("<br>","<b>","Bakery","</b>")
+
+var userData = prompt("Welcome to ABC Bakery. What to do order sir/ma'am?")
+
+var bakeryProduct = ["cake","apple pie","cookie","chips","patties"]
+
+if (userData === "cake") {
+    document.write("<br>","cake is","<b>"," available ","</b>","at index 0 in our bakery")
+}
+
+else if (userData === "apple pie") {
+    document.write("<br>","apple pie is","<b>"," available ","</b>","at index 1 in our bakery")
+}
+
+else if (userData === "cookie") {
+    document.write("<br>","cookie is","<b>"," available ","</b>","at index 2 in our bakery")
+}
+
+else if (userData === "chips") {
+    document.write("<br>","cake is","<b>"," available ","</b>","at index 3 in our bakery")
+}
+
+else if (userData === "patties") {
+    document.write("<br>","patties is","<b>"," available ","</b>","at index 4 in our bakery")
+}
+
+else{
+    document.write("<br>","We are Sorry.",userData," is ","<b>","not available ","</b>","in our bakery")
+}
+
+// ********************************Q5*****************************************
+
+var arr = [24,53,68,98]
+
+document.write("<br>","Array Items:",arr)
+
+var arr = [24,53,68,98]
+
+document.write("<br>","The largest number is 98")
+
+// ********************************Q5*****************************************
+
+var arr = [24,53,68,98]
+
+document.write("<br>","Array Items:",arr)
+
+var arr = [24,53,68,98]
+
+document.write("<br>","The largest number is 24" + "<br>")
+
+// ********************************Q5*****************************************
+
+for(var m = 1; m <= 100; m++){
+    document.write(5*m + ",")
+    
 }
